6a90d511f8a68ebf38027bfb55014a9f878c669b
steamwings <steamwings@outlook.com>
2019-11-15 11:28:28 -0500

    Checked out PizzaData from middledev

--- PizzaData/Models/Customers.cs
+++ PizzaData/Models/Customers.cs
@@ -1,12 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace PizzaData.Models
 {
     public partial class Customers
     {
         public int Id { get; set; }
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+        [Required]
+        [Column(TypeName = "char(44)")] // 256 bit
+        public string PasswordHash { get; set; }
+        [Required]
+        [Column(TypeName = "char(24)")] // 128 bit
+        public string Salt { get; set; }
+        [MinLength(2)]
         public string FirstName { get; set; }
+        [MinLength(2)]
         public string LastName { get; set; }
+
+        public virtual ICollection<Orders> Orders { get; set; }
     }
 }
