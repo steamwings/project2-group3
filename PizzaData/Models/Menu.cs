40e09fd862ba0e25d449be8b9860fc90b766608c
Kyle Robertson <kyle.robertson@revature.net>
2019-11-15 11:12:14 -0500

    Menu added w/ controller

--- PizzaData/Models/Menu.cs
+++ PizzaData/Models/Menu.cs
@@ -6,6 +6,10 @@ namespace PizzaData.Models
 {
     public partial class Menu
     {
+        public ICollection<CheeseTypes> Cheeses { get; set; }
+        public ICollection<CrustTypes> Crusts { get; set; }
+        public ICollection<SauceTypes> Sauces { get; set; }
         public ICollection<Sides> Sides { get; set; }
+        public ICollection<Toppings> Toppings { get; set; }
     }
 }
---
+++ PizzaShop/Controllers/MenuController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PizzaData.Models;
+
+namespace PizzaShop.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MenuController : ControllerBase
+    {
+        private readonly Project2DatabaseContext _context;
+
+        public MenuController(Project2DatabaseContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Menu
+        [HttpGet]
+        public async Task<ActionResult<Menu>> GetMenu()
+        {
+            return new Menu()
+            {
+                Cheeses = await _context.CheeseTypes.ToListAsync(),
+                Crusts = await _context.CrustTypes.ToListAsync(),
+                Sauces = await _context.SauceTypes.ToListAsync(),
+                Sides = await _context.Sides.ToListAsync(),
+                Toppings = await _context.Toppings.ToListAsync()
+            };
+        }
+    }
+}
