7df4ac69e5fcf892bd6aa90f90f2da101e131090
steamwings <steamwings@outlook.com>
2019-11-18 11:46:44 -0500

    Add crust/sauce/cheese selection to pizza builder. Doesn't actually work yet.

--- PizzaMvcUI/wwwroot/js/pizza.js
+++ PizzaMvcUI/wwwroot/js/pizza.js
@@ -1,56 +1,83 @@
 ﻿
-$(document).ready(function () {
-    assignGet("/api/cart/crust/");
-    makeRadioOptions("/api/crusttypes");
+$(document).ready(() => {
+    $("#btnIWant").click(crust);
 });
 
-function assignGet(uri) {
-    $("#btnIWant").click(function (e) {
-        var id = $("input[name=pizzaOption]:checked").val();
-        console.log("Read value is " + id);
-        $.ajax({
-//            contentType: 'application/json',
-            type: "GET",
-            url: baseUrl + uri + id,
-            success: function (data, textStatus, jqXHR) {
-                $("#headerSelect").val("Success!");
-                console.log("GET was successful...I think.");
-            },
-            error: function (jqXHR, textStatus, errorThrown) {
-                $("#headerSelect").val(jqXHR.statusText);
-                console.log("GET failed: " + textStatus);
-            }
-        });
+function crust() {
+    makeRadioOptions("/api/crusttypes");
+    addToProgressBar('Crust');
+    $("#btnIWant").val("I Want This!");
+    $("#btnIWant").unbind('click');
+    $("#btnIWant").click(sauce);
+}
+
+function sauce() {
+    addOptionToCart("/api/cart/crust/");
+    makeRadioOptions("/api/saucetypes");
+    addToProgressBar('Sauce');
+    $("#btnIWant").unbind('click');
+    $("#btnIWant").click(cheese);
+}
+
+function cheese() {
+    addOptionToCart("/api/cart/sauce/");
+    makeRadioOptions("/api/cheesetypes");
+    addToProgressBar('Cheese');
+    $("#btnIWant").unbind('click');
+    $("#btnIWant").click(toppings);
+}
+
+function toppings() {
+    addToProgressBar('Toppings');
+    $("#btnIWant").val("Whelp this doesn't work yet");
+    $("#btnIWant").unbind('click');
+}
+
+
+function addToProgressBar(name) {
+    var pbar = document.createElement('div');
+    pbar.setAttribute('class', 'progress-bar progress-bar-striped progress-bar-success active');
+    pbar.setAttribute('role', 'progressbar');
+    pbar.setAttribute('style', 'width:20%');
+    pbar.innerHTML = name;
+    $("#progressBuildPizza").append(pbar);
+}
+
+function addOptionToCart(uri) {
+    var id = $("input[name=pizzaOption]:checked").val();
+    console.log("Read value is " + id);
+    $.ajax({
+        //            contentType: 'application/json',
+        type: "GET",
+        url: baseUrl + uri + id,
+        success: function (data, textStatus, jqXHR) {
+            $("#headerSelect").val("Success!");
+            console.log("GET was successful...I think.");
+        },
+        error: function (jqXHR, textStatus, errorThrown) {
+            $("#headerSelect").val(jqXHR.statusText);
+            console.log("GET failed: " + textStatus);
+        }
     });
 }
 
 function makeRadioOptions(resource, headerText) {
+    makeOptions(resource, headerText, populateRadio);
+}
+
+function makeOptions(resource, headerText, callback) {
+    $("#divDoStuff").innerHTML = 'Loading...';
     $.ajax({
         contentType: 'application/json',
         type: "GET",
         url: apiUrl + resource,
-//        headers: { "Access-Control-Request-Method": "GET"},
- //       xhrFields: { withCredentials: true },
-        success: function (data, textStatus, jqXHR) {
+        //        headers: { "Access-Control-Request-Method": "GET"},
+        //       xhrFields: { withCredentials: true },
+        success: (data, textStatus, jqXHR) => {
             console.log("API GET succeeded");
-            document.getElementsByName("pizzaOption").forEach(function (e) { e.remove() });
-            data.forEach(function (option) {
-                var d = document.createElement('div');
-                var o = document.createElement('input');
-                o.setAttribute('type', 'radio');
-                o.setAttribute('name', 'pizzaOption');
-                o.setAttribute('id', option.name);
-                o.setAttribute('value', option.id);
-                $("#divDoStuff").append(o);
-                var l = document.createElement('label');
-                l.setAttribute('for', option.name);
-                l.setAttribute('style', 'style="display:block"');
-                l.innerText = " " + option.name + " - " + option.description;
-                $(d).append(o);
-                $(d).append(l);
-                $("#divDoStuff").prepend(d);
-            });
             $("#headerSelect").val(headerText);
+            $("#divDoStuff").innerHTML = '';
+            callback(data);
         },
         error: function (jqXHR, textStatus, errorThrown) {
             console.log("API GET failed");
@@ -59,8 +86,23 @@ function makeRadioOptions(resource, headerText) {
     });
 }
 
-//<form>
-//    <input type="radio" name="gender" value="male" checked> Male<br>
-//        <input type="radio" name="gender" value="female"> Female<br>
-//            <input type="radio" name="gender" value="other"> Other  
-//</form> 
+function populateRadio (data) {
+    var groupName = 'pizzaDiv';
+    $("#divDoStuff").empty();
+    data.forEach(function (option) {
+        var d = document.createElement('div');
+        d.setAttribute('name', groupName);
+        var o = document.createElement('input');
+        o.setAttribute('type', 'radio');
+        o.setAttribute('name', 'pizzaOption');
+        o.setAttribute('id', option.name);
+        o.setAttribute('value', option.id);
+        var l = document.createElement('label');
+        l.setAttribute('for', option.name);
+        l.setAttribute('style', 'style="display:block"');
+        l.innerHTML = " <strong>" + option.name + "</strong> - " + option.description;
+        $(d).append(o);
+        $(d).append(l);
+        $("#divDoStuff").prepend(d);
+    });
+}
