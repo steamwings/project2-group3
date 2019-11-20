e2f03303a71c09af0cf6849e9ff8ffe1fa414cff
Robert W <robertwideman94@gmail.com>
2019-11-19 21:09:02 -0500

    added view for a cart

---
+++ PizzaMvcUI/Models/CartVM.cs
@@ -0,0 +1,13 @@
+﻿using PizzaData.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PizzaMvcUI.Models
+{
+    public class CartVM
+    {
+        public Cart Cart { get; set; }
+        public Menu Menu { get; set; }
+    }
+}
